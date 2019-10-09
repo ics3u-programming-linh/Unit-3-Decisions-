b6fcf74d6e5768e3bcdd475e64e985363d6f0000
Linh <46288798+s272936@users.noreply.github.com>
2019-10-09 12:57:12 -0400

    FL

---
+++ FactorialLinhH/FactorialLinhH/FactorialForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace FactorialLinhH
+{
+    partial class frmFactorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstFactorialNumbers = new System.Windows.Forms.ListBox();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblFactorialAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(256, 147);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(134, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lstFactorialNumbers
+            // 
+            this.lstFactorialNumbers.FormattingEnabled = true;
+            this.lstFactorialNumbers.Location = new System.Drawing.Point(190, 203);
+            this.lstFactorialNumbers.Name = "lstFactorialNumbers";
+            this.lstFactorialNumbers.Size = new System.Drawing.Size(250, 160);
+            this.lstFactorialNumbers.TabIndex = 2;
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(163, 92);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(82, 13);
+            this.lblNumber.TabIndex = 4;
+            this.lblNumber.Text = "Enter a number:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(276, 92);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 5;
+            // 
+            // lblFactorialAnswer
+            // 
+            this.lblFactorialAnswer.AutoSize = true;
+            this.lblFactorialAnswer.Location = new System.Drawing.Point(201, 227);
+            this.lblFactorialAnswer.Name = "lblFactorialAnswer";
+            this.lblFactorialAnswer.Size = new System.Drawing.Size(81, 13);
+            this.lblFactorialAnswer.TabIndex = 6;
+            this.lblFactorialAnswer.Text = "factorial answer";
+            // 
+            // frmFactorial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFactorialAnswer);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lstFactorialNumbers);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmFactorial";
+            this.Text = "Factorial by Linh H";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstFactorialNumbers;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblFactorialAnswer;
+    }
+}
+
---
+++ FactorialLinhH/FactorialLinhH/FactorialForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialLinhH
+{
+    public partial class frmFactorial : Form
+    {
+        public frmFactorial()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            double factorialAnswer;
+            double factorialNumber;
+            int factorialCounter;
+
+            // clear the items from the listbox
+            this.lstFactorialNumbers.Items.Clear();
+
+            // initialize the final answer to 1
+            
+            // get the number from the user
+            factorialNumber = Convert.ToDouble(this.txtNumber.Text);
+
+            // set the counter to 0
+
+            // multiply the counter by the next incremented number until it reaches the user's number
+            do
+            {
+                // increment the counter by 1
+
+                // list the counter number in the listbox for the user to see
+
+                // multiply the counter by the answer
+            } while ();
+
+            // convert the factorialAnswer to a String and insert it into the label
+            this.lblFactorialAnswer.Text = this.txtNumber.Text + "! = " + Convert.ToString(factorialAnswer);
+        }
+    }
+}
