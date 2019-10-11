1585046a69c1cfbc1e188429b6f12a9fa7a96717
Linh <46288798+s272936@users.noreply.github.com>
2019-10-10 23:59:00 -0400

    WM

---
+++ WalkingManLinhH/WalkingManLinhH/WalkingManForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WalkingManLinhH
+{
+    partial class frmWalkingMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWalk = new System.Windows.Forms.Button();
+            this.picMan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnWalk
+            // 
+            this.btnWalk.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWalk.Location = new System.Drawing.Point(277, 61);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(191, 45);
+            this.btnWalk.TabIndex = 0;
+            this.btnWalk.Text = "Walk";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            this.btnWalk.Click += new System.EventHandler(this.BtnWalk_Click);
+            // 
+            // picMan
+            // 
+            this.picMan.Image = global::WalkingManLinhH.Properties.Resources.walk10;
+            this.picMan.Location = new System.Drawing.Point(252, 137);
+            this.picMan.Name = "picMan";
+            this.picMan.Size = new System.Drawing.Size(245, 243);
+            this.picMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMan.TabIndex = 1;
+            this.picMan.TabStop = false;
+            // 
+            // frmWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picMan);
+            this.Controls.Add(this.btnWalk);
+            this.Name = "frmWalkingMan";
+            this.Text = "Walking Man by Linh H";
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWalk;
+        private System.Windows.Forms.PictureBox picMan;
+    }
+}
+
