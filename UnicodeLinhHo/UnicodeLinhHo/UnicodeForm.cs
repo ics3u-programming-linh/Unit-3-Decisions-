8738f3adcf8b558f7d74b4f99b40e0fa068b4996
Linh <46288798+s272936@users.noreply.github.com>
2019-10-17 13:10:46 -0400

    u

---
+++ UnicodeLinhHo/UnicodeLinhHo/UnicodeForm.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by: First Last
+ * Created on: Day-Month-Year
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day # - Name of Program
+ * This program...
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeLinhHo
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            double 
+    
+            int 
+            // declare string and counter variables
+            // clear the items in the listbox
+            // create loop to convert each number
+                // convert the counter to unicode leter
+                // add letter with its value to the listbox
+
+        }
+    }
+}
