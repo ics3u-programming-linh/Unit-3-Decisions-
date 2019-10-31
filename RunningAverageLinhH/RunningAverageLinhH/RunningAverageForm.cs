f9f35d3c386652d0bcb7ea9954e757beedd924bd
Linh <46288798+s272936@users.noreply.github.com>
2019-10-31 13:02:56 -0400

    Update RunningAverageForm.cs

--- RunningAverageLinhH/RunningAverageLinhH/RunningAverageForm.cs
+++ RunningAverageLinhH/RunningAverageLinhH/RunningAverageForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Linh Ho
+ * Created on: October 31st, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #29 - Running Average
+ * This program allows the user to input a number and it calculate the average.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,17 +20,23 @@ namespace RunningAverageLinhH
 {
     public partial class frmRunningAverage : Form
     {
-        // declare the constants and variables
+        // set the variables
         double sum = 0;
         double numbersByNumbers = 0;
 
         public frmRunningAverage()
         {
             InitializeComponent();
+
+            // hide the labels
+            lblAverageAnswer.Hide();
         }
 
         private void BtnAverage_Click(object sender, EventArgs e)
         {
+            // Show the labels
+            lblAverageAnswer.Show();
+
             // declare local variables
             double userValue;
             double average;
@@ -40,7 +54,14 @@ namespace RunningAverageLinhH
             // display the average of the running
             lblAverageAnswer.Text = Convert.ToString(average);
 
-
+            // if the user enters -1, display a Goodbye message and disable buttons
+            if (userValue == -1)
+            {
+                MessageBox.Show("Running average ended.", "Running Average");
+                this.btnAverage.Enabled = false;
+                this.txtNumber.Visible = false;
+                this.Close();
+            }
 
         }
     }
